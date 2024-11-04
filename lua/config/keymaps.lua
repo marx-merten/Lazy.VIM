51fe578c5eab85ad12c60e01621b176ba94a6458
Stefan Marx <stefan@marx-merten.de>
2024-11-04 12:12:54 +0100

    base config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Basics
+-- Motion
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Page up keep Cursor centered", remap = true })
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Page down keep Cursor centered", remap = true })
+vim.keymap.set("n", "<leader>U", "<cmd>Telescope undo<cr>")
+
+--
+-- Overrides
