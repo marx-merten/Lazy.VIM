1460ba695671f9e1fd65538c0e14b04249701b7a
Stefan Marx <stefan@marx-merten.de>
2024-11-19 13:06:26 +0100

    move to new dashboard and enable projects

---
+++ lua/plugins/snacks-dashboard.lua
@@ -0,0 +1,49 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+    _____       _ _         
+   |     |___ _| |_|___ ___
+   |   --| . | . | |   | . |
+   |_____|___|___|_|_|_|_  |
+ ___                   |___|   _
+|  _|___ ___ ___ _____ ___ ___| |_ ___  
+|  _|  _| .'| . |     | -_|   |  _|_ -| 
+|_| |_| |__,|_  |_|_|_|___|_|_|_| |___| 
+            |___|                       
+                                        
+    ]],
+        -- -- stylua: ignore
+        -- ---@type snacks.dashboard.Item[]
+        -- keys = {
+        --   { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+        --   { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+        --   { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+        --   { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+        --   { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+        --   { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+        --   { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+        --   { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+        --   { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        -- },
+      },
+      formats = {
+        header = { "%s", align = "left" },
+      },
+      sections = {
+        {
+          { section = "header" },
+        },
+        {
+          pane = 2,
+          { section = "keys", gap = 1, padding = 1 },
+          -- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
+      },
+    },
+  },
+}
