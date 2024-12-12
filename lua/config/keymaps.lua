e60c6a0ca516ec7fdac9e994eb6af6a8bf3dbcb7
Stefan Marx <stefan@marx-merten.de>
2024-12-12 11:06:45 +0100

    remove the tmux navigation if used in vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,19 +10,21 @@ vim.keymap.set("n", "<leader>U", "<cmd>Telescope undo<cr>")
 
 map("n", "<leader>ghP", "<cmd>Gitsigns preview_hunk<cr>", { desc = "Preview Hunk" })
 -- colemak related navigation Overrides
+
 -- using cursor keys on navlayer
-map("n", "<C-Left>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Go to Left Window", remap = true })
-map("n", "<C-Down>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Go to Lower Window", remap = true })
-map("n", "<C-Up>", "<cmd>NvimTmuxNavigateUp<cr>", { desc = "Go to Upper Window", remap = true })
-map("n", "<C-Right>", "<cmd>NvimTmuxNavigateRight<cr>", { desc = "Go to Right Window", remap = true })
+if not vim.g.vscode then
+  map("n", "<C-Left>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Go to Left Window", remap = true })
+  map("n", "<C-Down>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Go to Lower Window", remap = true })
+  map("n", "<C-Up>", "<cmd>NvimTmuxNavigateUp<cr>", { desc = "Go to Upper Window", remap = true })
+  map("n", "<C-Right>", "<cmd>NvimTmuxNavigateRight<cr>", { desc = "Go to Right Window", remap = true })
 
-map("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", {})
-map("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", {})
-map("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", {})
-map("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", {})
-map("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<cr>", {})
-map("n", "<C-Space>", "<cmd>NvimTmuxNavigateNext<cr>", {})
-
+  map("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", {})
+  map("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", {})
+  map("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", {})
+  map("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", {})
+  map("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<cr>", {})
+  map("n", "<C-Space>", "<cmd>NvimTmuxNavigateNext<cr>", {})
+end
 map("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer", remap = true })
 map("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "<A-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
@@ -31,7 +33,6 @@ map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
 map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 map("v", "<A-Down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 map("v", "<A-Up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
-
 -- Autocompletion
 
 Snacks.toggle
