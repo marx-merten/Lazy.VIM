cb82ee8c4387af32eadd5877a8b719c74595300b
Stefan Marx <stefan@marx-merten.de>
2025-08-21 23:52:06 +0200

    enabled venv selector

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,16 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    -- "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python",
+    -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+    { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "venv-selector" },
+  },
+  opts = {
+    -- Your settings go here
+  },
+}
