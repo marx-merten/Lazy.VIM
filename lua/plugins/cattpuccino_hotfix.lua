eb9cd849f2646965be472a2bb2aa73702b6b6d26
Stefan Marx <stefan@marx-merten.de>
2025-08-22 08:51:57 +0200

    slightly better version that doesn't force cattpuccin to be loaded

--- lua/plugins/cattpuccino_hotfix.lua
+++ lua/plugins/cattpuccino_hotfix.lua
@@ -1,21 +1,31 @@
+-- return {
+--   "akinsho/bufferline.nvim",
+--   -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
+--   init = function()
+--     local bufline = require("catppuccin.groups.integrations.bufferline")
+--     bufline.get = bufline.get_theme
+--   end,
+--   ---@module 'bufferline'
+--   ---@type bufferline.Config
+--   -- opts = {
+--   --   options = {
+--   --     always_show_bufferline = true,
+--   --     separator_style = "thick",
+--   --     hover = {
+--   --       enabled = true,
+--   --       delay = 120,
+--   --       reveal = { "close" },
+--   --     },
+--   --   },
+--   -- },
+-- }
+--
+
 return {
-  "akinsho/bufferline.nvim",
-  -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
-  init = function()
-    local bufline = require("catppuccin.groups.integrations.bufferline")
-    bufline.get = bufline.get_theme
+  "catppuccin",
+  optional = true,
+  opts = function()
+    local bufferline = require("catppuccin.groups.integrations.bufferline")
+    bufferline.get = bufferline.get or bufferline.get_theme
   end,
-  ---@module 'bufferline'
-  ---@type bufferline.Config
-  -- opts = {
-  --   options = {
-  --     always_show_bufferline = true,
-  --     separator_style = "thick",
-  --     hover = {
-  --       enabled = true,
-  --       delay = 120,
-  --       reveal = { "close" },
-  --     },
-  --   },
-  -- },
 }
